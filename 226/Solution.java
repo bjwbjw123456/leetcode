23340fbcb57c9175c0f6bd6591697e27d75d8ab2
bjwbjw123456 <bjwbjw770@gmail.com>
2015-12-16 20:36:26 -0600

    add 226

---
+++ 226/Solution.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode l = root.left;
+        TreeNode r = root.right;
+        root.left = invertTree(r);
+        root.right = invertTree(l);
+        return root;
+    }
+}
