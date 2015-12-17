be5fed682f94522fc9daacc5f5eb4abd973dfe55
bjwbjw123456 <bjwbjw770@gmail.com>
2015-12-17 15:01:47 -0600

    add 101

---
+++ 101/Solution.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        return helper(root.left,root.right);
+        
+        
+    }
+    
+    private boolean helper(TreeNode l, TreeNode r) {
+        if (l==null && r == null) {
+            return true;
+        }
+        if ( (l==null && r!=null)||(r==null && l!=null) ) {
+            return false;
+        }
+        if (l.val == r.val) {
+            return ( helper(l.left,r.right) && helper(l.right,r.left) );
+        } else {
+            return false;
+        }
+            
+    }
+    
+}
