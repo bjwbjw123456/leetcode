8327cb5af2bea8bdc871ea40c3ed611d0e82f70e
bjwbjw123456 <bjwbjw770@gmail.com>
2015-12-20 18:41:18 -0600

    changed 287

--- 287/Solution.java
+++ 287/Solution.java
@@ -10,6 +10,21 @@ public class Solution {
         return count;
     }
     
+    private int helper(int[] nums, int l, int r) {
+        int k = (l+r)/2;
+        int count = counting(nums,k);
+        if (count>=k+1) {
+            return helper(nums,l,k);
+        } else {
+            int count1 = counting(nums,k+1);
+            if (count1>=k+2) {
+                return k+1;
+            } else {
+                return helper(nums,k,r);
+            }
+        }
+    }
+    
     public int findDuplicate(int[] nums) {
         if (nums.length == 2) {
             return nums[0];
@@ -18,12 +33,6 @@ public class Solution {
         int count;
         
         int n = l-1;
-        for (int i=n;i>=1;i--){
-            count = counting(nums,i);
-            if (count<i+1) {
-                return i+1;
-            }
-        }
-        return 1;
+        return helper(nums,0,n);
     }
 }
