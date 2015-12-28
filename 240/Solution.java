fa015c8570d04ea40e219dd39a65ed738dfdc7d4
bjwbjw123456 <bjwbjw770@gmail.com>
2015-12-28 16:56:47 -0600

    add 240

---
+++ 240/Solution.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0) return false;
+        if(matrix[0].length == 0) return false;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        int i = m-1;
+        int j = 0;
+        
+        while( i>=0&&i<m&&j>=0&&j<n ) {
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            else if (matrix[i][j] < target) {
+                j++;
+            } else {
+                i--;
+            }
+        }
+        return false;
+    }
+}
