ab0f967ce6cc4af92aa052bc088533891ecff3be
bjwbjw123456 <bjwbjw770@gmail.com>
2016-01-01 16:19:43 -0600

    add 111

---
+++ 111/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.right==null && root.left==null) return 1;
+        if (root.left!=null && root.right==null) {
+            return minDepth(root.left) + 1;
+        }
+        if (root.left==null && root.right!=null) {
+            return minDepth(root.right) + 1;
+        }
+        
+        return Math.min(minDepth(root.left),minDepth(root.right))+1;
+    }
+}
