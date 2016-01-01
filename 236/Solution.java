f8bf563d82af49e28dd73885577e006537c98078
bjwbjw123456 <bjwbjw770@gmail.com>
2016-01-01 14:57:09 -0600

    add 236

---
+++ 236/Solution.java
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) return null;
+        if (p == root|| q==root) return root;
+        TreeNode l = lowestCommonAncestor(root.left,p,q);
+        TreeNode r = lowestCommonAncestor(root.right,p,q);
+        if (l!=null && r!=null) return root;
+        return (l!=null)?l:r;
+        
+    }
+}
