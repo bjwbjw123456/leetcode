30810c7348f28937fa9387fce8f28efba57dc0fa
bjwbjw123456 <bjwbjw770@gmail.com>
2016-01-15 15:53:14 -0600

    add 46

---
+++ 46/Solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+    private void helper(int[] nums, List<List<Integer>> r, List<Integer> cur) {
+        if (nums.length == 0) r.add(cur);
+        for (int i=0;i<nums.length;i++) {
+            List<Integer> newone = new ArrayList<Integer>(cur);
+            newone.add(nums[i]);
+            int[] newnums = new int[nums.length-1];
+            for (int j=0;j<nums.length;j++) {
+                if (j<i) newnums[j] = nums[j];
+                else if (j>i) newnums[j-1] = nums[j];
+                else continue;
+            }
+            helper(newnums,r,newone);
+        }
+        
+    }
+    
+    
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> r = new ArrayList<List<Integer>>();
+        if (nums==null || nums.length ==0) {
+            return r;
+        } 
+        int l = nums.length;
+        List<Integer> cur = new ArrayList<Integer>();
+        helper(nums,r,cur);
+        return r;
+    }
+}
