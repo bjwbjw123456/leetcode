91e61f1e71e1ea32880811ad2913e7072dde2c6a
bjwbjw123456 <bjwbjw770@gmail.com>
2016-01-16 15:59:30 -0600

    add 39

---
+++ 39/Solution.java
@@ -0,0 +1,39 @@
+public class Solution {
+    List<List<Integer>> r;
+    
+    private void helper(int[] nums, int start, int count, int target, List<Integer> cur) {
+        /*if (start==nums.length-1) {
+            if (nums[start] == target-count) {
+                List<Integer> tmp = new ArrayList<Integer>(cur);
+                tmp.add(nums[start]);
+                r.add(tmp);
+            }
+            return;
+        }*/
+        for (int i=start;i<nums.length;i++) {
+            if (nums[i]>target-count) {
+                return;
+            }
+            if (nums[i]==target-count) {
+                List<Integer> tmp = new ArrayList<Integer>(cur);
+                tmp.add(nums[i]);
+                r.add(tmp);
+                return;
+            }
+            List<Integer> tmp = new ArrayList<Integer>(cur);
+            tmp.add(nums[i]);
+            helper(nums,i,count+nums[i],target,tmp);
+        }
+        return;
+    }
+    
+    
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        r = new ArrayList<List<Integer>>();
+        if (candidates==null || candidates.length==0) return r;
+        Arrays.sort(candidates);
+        List<Integer> cur = new ArrayList<Integer>();
+        helper(candidates,0,0,target,cur);
+        return r;
+    }
+}
