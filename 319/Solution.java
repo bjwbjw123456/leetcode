a162c3a3498a6e537fb64aedb7564ec7513db304
bjwbjw123456 <bjwbjw770@gmail.com>
2016-01-26 11:30:32 -0600

    add 319

---
+++ 319/Solution.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int bulbSwitch(int n) {
+        int count = 0;
+        for (int i=1;i*i<=n;i++) {
+            count++;
+        }
+        return count;
+    }
+}
